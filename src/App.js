e6643f02c36545180fe54bc850c3941da7ced1a9
Amjad-Almohtaseb <amjad.almohtaseb94@gmail.com>
2021-06-08 03:14:10 +0300

    finaledit

--- src/App.js
+++ src/App.js
@@ -1,7 +1,8 @@
 import React from "react";
 import { useState } from "react";
-//components
-import RandomGen from "./components/RandomGen";
+
+//styling
+import "./App.css";
 
 function App() {
   const [userNumber, setUserNumber] = useState();
@@ -10,7 +11,7 @@ function App() {
 
   const [attempts, setAttempts] = useState(0);
 
-  const [clickedNum, setclickedNum] = useState();
+  const [clickedNumber, setclickedNumber] = useState();
 
   const [gameOver, setGameOver] = useState(false);
 
@@ -18,19 +19,11 @@ function App() {
     if (event.target.value.length < 3 || event.target.value == 100) {
       setUserNumber(event.target.value);
     }
-
-    console.log(event.target.value);
   };
 
   const compare = () => {
     if (userNumber != "") {
-      setclickedNum(userNumber);
-
-      console.log("R", random);
-
-      if (random == userNumber) {
-        console.log("Winer");
-      }
+      setclickedNumber(userNumber);
 
       setAttempts(attempts + 1);
       if (attempts == 5) {
@@ -48,51 +41,50 @@ function App() {
     setRandom(Math.floor(Math.random() * 100 + 1));
     setAttempts(0);
     setUserNumber();
-    setclickedNum();
+    setclickedNumber();
   };
 
   const fullAttempt = () => {
     reset();
     setGameOver(false);
   };
+  console.log("randomNumber", random);
 
   return (
-    <center>
-      <div>
-        <p>
-          The Monster stirs beneath the tumultuous waves, thinking of a number
-          that will end the world Find the secret number and use it to banish
-          The Monster
-        </p>
-        <input
-          placeholder="1 - 100"
-          type="number"
-          min="1"
-          maxLength="3"
-          required
-          value={userNumber}
-          onChange={handleChange}
-          style={{ maxLength: 3 }}
-        />
-        <button type="submit" onClick={compare}>
-          Submit
-        </button>{" "}
-        <br />
-        <br />
-        <button type="reset" onClick={fullAttempt}>
-          Reset
-        </button>
-        {clickedNum > random ? <p> too strong </p> : <p></p>}
-        {clickedNum < random ? <p> too weak </p> : <p></p>}
-        {clickedNum == random ? (
-          <p> "Correct Answer" </p>
-        ) : (
-          <p> {console.log(random)}</p>
-        )}
-        <div>{gameOver && <b> Game over </b>}</div>
-        <p> attempts : {attempts} </p>
-      </div>
-    </center>
+    <div className="container">
+      <h1>
+        Katkoot Monster <br />
+        stirs beneath the tumultuous waves, thinking of a number that will end
+        the world{" "}
+      </h1>
+      <h2>Find the secret number and use it to banish The Katkoot Monster</h2>
+      <input
+        placeholder="1 - 100"
+        type="number"
+        min="1"
+        maxLength="3"
+        required
+        value={userNumber}
+        onChange={handleChange}
+        style={{ maxLength: 3 }}
+      />
+      <br />
+      <br />
+      <button type="submit" onClick={compare}>
+        banish
+      </button>{" "}
+      <br />
+      <br />
+      <button type="reset" onClick={fullAttempt}>
+        Reset
+      </button>
+      <br />
+      {clickedNumber > random ? <b> too strong </b> : <p></p>}
+      {clickedNumber < random ? <b> too weak </b> : <p></p>}
+      {clickedNumber == random ? <b> "Correct Answer" </b> : <p> </p>}
+      <div>{gameOver && <b> Game over </b>}</div>
+      <b> attempts : {attempts} </b>
+    </div>
   );
 }
 
--- src/components/RandomGen.js
+++
@@ -1,6 +0,0 @@
-let successNumber = Math.floor(Math.random() * 100 + 1);
-const RandomGen = (props) => {
-  return props.userNumber;
-};
-
-export default RandomGen;
